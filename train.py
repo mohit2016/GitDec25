528d5e412d3073e7e77afd2ba3cf1789399c4330
Mohit Uniyal <mohituniyal2010@gmail.com>
2025-12-04 08:15:09 +0530

    train DT model

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score
@@ -11,7 +11,7 @@ X, y = make_classification(n_samples=1000, n_features=20, n_classes=2, random_st
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
-model = LogisticRegression(random_state=42)
+model = DecisionTreeClassifier(random_state=42)
 model.fit(X_train, y_train)
 
 # Save model
